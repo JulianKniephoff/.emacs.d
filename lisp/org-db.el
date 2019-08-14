f259cc56cbe1d638fe5fe9a53c10eff85d69c1fe
Julian Kniephoff <me@juliankniephoff.com>
2019-08-14 21:27:06 +0200

    first seedling of `org-db`

---
+++ lisp/org-db.el
@@ -0,0 +1,27 @@
+(require 'org-id)
+(require 'org-element-x)
+
+(defvar org-db-file (expand-file-name "db.org" org-directory))
+
+(defun org-db-insert (&optional element interactive)
+  (interactive "i\np")
+  (print element)
+  (print interactive)
+  (when (not element)
+    (setq element (org-element-up 'headline (org-element-at-point))))
+  (let ((copy (org-element-deep-copy element 'headline))
+        (id (or (org-element-property :ID element)
+                (org-id-uuid))))
+    (org-element-set-property element "SOURCE_ID" id)
+    (org-element-remove-property element "ID")
+    (when interactive
+      (org-delete-property "ID")
+      (org-set-property "SOURCE_ID" id))
+    (org-element-set-property copy "ID" id)
+    (org-element-put-property copy :level 1)
+    (save-window-excursion
+      (find-file org-db-file)
+      (goto-char (point-max))
+      (insert (org-element-interpret-data copy)))))
+
+(provide 'org-db)
