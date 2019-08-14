63773669805dc1d7a989d219a541d22e611b8229
Julian Kniephoff <me@juliankniephoff.com>
2019-08-14 21:26:45 +0200

    add some useful `org-element` extensions

---
+++ lisp/org-element-x.el
@@ -0,0 +1,60 @@
+(require 'seq)
+
+(defun org-element-property-name-to-keyword (property)
+  (intern (concat ":" property)))
+
+(defun org-element-deep-copy (element &optional boundary)
+  (let ((boundary (if (listp boundary) boundary (list boundary))))
+    (apply #'org-element-adopt-elements
+           (org-element-copy element)
+           (mapcar #'org-element-deep-copy
+                   (seq-filter (lambda (element)
+                                 (not (memq (org-element-type element)
+                                            boundary)))
+                               (org-element-contents element))))))
+
+(defun org-element-set-property (element property value)
+  (org-element-put-property element
+                            (org-element-property-name-to-keyword
+                             property)
+                            value)
+  (let ((node-property `(node-property
+                         (:key ,property :value ,value))))
+    (if-let ((property-drawer
+              (org-element-map
+                  (org-element-contents element)
+                  'property-drawer
+                #'identity nil 'first-match 'headline)))
+        (if-let ((existing-property
+                  (org-element-map property-drawer 'node-property
+                    (lambda (p)
+                      (when (string= (org-element-property :key p)
+                                     property)
+                        p))
+                    nil 'first-match)))
+            (org-element-put-property existing-property :value value)
+          (org-element-adopt-elements property-drawer node-property))
+      (let ((property-drawer
+             (list 'property-drawer nil node-property)))
+        (if-let ((contents (org-element-contents element)))
+            (org-element-insert-before property-drawer (car contents))
+          (org-element-adopt-elements element property-drawer))))))
+
+(defun org-element-up (types element)
+  (while (and element
+              (not (memq (org-element-type element)
+                         (if (listp types) types (list types)))))
+    (setq element (org-element-property :parent element)))
+  element)
+
+(defun org-element-remove-property (element property)
+  (org-element-map element 'node-property
+    (lambda (p)
+      (when (string= property (org-element-property :key p))
+        (org-element-extract-element p))))
+  (org-element-put-property element
+                            (org-element-property-name-to-keyword
+                             property)
+                            nil))
+
+(provide 'org-element-x)
