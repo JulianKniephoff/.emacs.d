45742e2da827ceef205a69a2ef2bf06a85adecb8
Julian Kniephoff <me@juliankniephoff.com>
2020-08-11 15:10:18 +0200

    updates

--- .cask/26.3/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
+++ .cask/26.3/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "b26c35620cdbf8dcf068edd2d32a4f1aff0e4f61")
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "bb1b37ea3bbf3ad3b513ad8642ef514099699aac")
--- .cask/26.3/elpa/cmake-mode-20190710.1319/cmake-mode.el
+++ .cask/26.3/elpa/cmake-mode-20190710.1319/cmake-mode.el
@@ -2,7 +2,7 @@
 
 ;; Package-Requires: ((emacs "24.1"))
 ;; Package-Version: 20190710.1319
-;; Package-Commit: b26c35620cdbf8dcf068edd2d32a4f1aff0e4f61
+;; Package-Commit: bb1b37ea3bbf3ad3b513ad8642ef514099699aac
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
