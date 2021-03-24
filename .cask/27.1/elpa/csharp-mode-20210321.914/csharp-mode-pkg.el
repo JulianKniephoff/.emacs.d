02d47b2dd4dbf9a11c32b10bfff88b3b37777c4a
Julian Kniephoff <me@juliankniephoff.com>
2021-03-24 07:37:20 +0100

    updates

---
+++ .cask/27.1/elpa/csharp-mode-20210321.914/csharp-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "csharp-mode" "20210321.914" "C# mode derived mode"
+  '((emacs "26.1")
+    (tree-sitter "0.12.1")
+    (tree-sitter-indent "0.1")
+    (tree-sitter-langs "0.9.1"))
+  :commit "f977800161ccdb58d5650a8ca60017c83863d80a" :authors
+  '(("Theodor Thornhill" . "theo@thornhill.no"))
+  :maintainer
+  '("Jostein Kjønigsen" . "jostein@gmail.com")
+  :keywords
+  '("c#" "languages" "oop" "mode")
+  :url "https://github.com/emacs-csharp/csharp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/27.1/elpa/floobits-20180801.524/floobits-pkg.el
@@ -0,0 +1,14 @@
+(define-package "floobits" "20180801.524" "Floobits plugin for real-time collaborative editing"
+  '((json "1.2")
+    (highlight "0"))
+  :commit "489b294a7f30ecd2af2edc0823dead8102f27af6" :authors
+  '(("Matt Kaniaris")
+    ("Geoff Greer"))
+  :maintainer
+  '("Matt Kaniaris")
+  :keywords
+  '("comm" "tools")
+  :url "http://github.com/Floobits/floobits-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
