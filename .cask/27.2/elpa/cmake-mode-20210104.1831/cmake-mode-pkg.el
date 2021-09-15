94b3de34d5fb9e7a4857e77c9a641c9eddb88d6a
Julian Kniephoff <me@juliankniephoff.com>
2021-09-15 17:56:15 +0200

    updates

--- .cask/27.2/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
+++ .cask/27.2/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "69c0a5daf98958ba6ad4311781ec72fe8b9bfb0a")
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "c2c1c701c7f0768c382c819b6922b134690212ba")
