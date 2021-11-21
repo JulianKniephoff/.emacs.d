f962652090924660c296d7de63565855eedd3aa2
Julian Kniephoff <me@juliankniephoff.com>
2021-11-21 15:31:31 +0100

    enable `org-agenda` hotkey

--- init.el
+++ init.el
@@ -157,7 +157,8 @@
 
 (use-package org
   :mode ("\\.org\\'" . org-mode)
-  :bind ([?\C-c ?l] . #'org-store-link)
+  :bind (([?\C-c ?l] . #'org-store-link)
+	 ([?\C-c ?a] . #'org-agenda))
   :config
 
   (use-package org-inlinetask)
