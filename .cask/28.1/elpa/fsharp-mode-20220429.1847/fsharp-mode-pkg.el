8a930c40df82b023d7381d570a9398643e560fc5
Julian Kniephoff <me@juliankniephoff.com>
2022-04-30 15:57:47 +0200

    updates

---
+++ .cask/28.1/elpa/fsharp-mode-20220429.1847/fsharp-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "fsharp-mode" "20220429.1847" "Support for the F# programming language"
+  '((emacs "25"))
+  :commit "5208b54098c7534f4768b87c5f4c8a01b638737b" :authors
+  '(("1993-1997 Xavier Leroy, Jacques Garrigue and Ian T Zimmerman")
+    ("2010-2011 Laurent Le Brun" . "laurent@le-brun.eu")
+    ("2012-2014 Robin Neatherway" . "robin.neatherway@gmail.com")
+    ("2017-2022 Jürgen Hötzel"))
+  :maintainer
+  '("Jürgen Hötzel")
+  :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
