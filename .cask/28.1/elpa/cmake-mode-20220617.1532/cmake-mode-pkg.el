110f69ce93fd3c61c308a205ff357cf424a506f5
Julian Kniephoff <me@juliankniephoff.com>
2022-07-06 13:10:39 +0200

    updates

--- .cask/28.1/elpa/cmake-mode-20220617.1532/cmake-mode-pkg.el
+++ .cask/28.1/elpa/cmake-mode-20220617.1532/cmake-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20220617.1532" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "ac977c52a697bfc13576a222c229606eaedd9154")
+(define-package "cmake-mode" "20220617.1532" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "d7ba536841b07d152103797fdf187c14b6b716de")
---
+++ .cask/28.1/elpa/typescript-mode-20220705.2151/typescript-mode-pkg.el
@@ -0,0 +1,8 @@
+(define-package "typescript-mode" "20220705.2151" "Major mode for editing typescript"
+  '((emacs "24.3"))
+  :commit "3eec4032a4592c7cb9798db8556bad92ea5a4f4b" :keywords
+  '("typescript" "languages")
+  :url "http://github.com/ananthakumaran/typescript.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
