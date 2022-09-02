e85c203ba5f6f914a756fbb03891f9a95262b952
Julian Kniephoff <me@juliankniephoff.com>
2022-09-02 13:40:36 +0200

    updates

--- .cask/28.1/elpa/clojure-mode-20220824.1801/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from clojure-mode.el  -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20220824.1801" "Major mode for Clojure code" '((emacs "25.1")) :commit "905abd0b42b081762c2df14015b5be49b206f53c" :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
