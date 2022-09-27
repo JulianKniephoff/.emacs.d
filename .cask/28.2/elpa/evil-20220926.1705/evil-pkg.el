a8287a79e9cfdc0e2a7486eb337eee0208c46078
Julian Kniephoff <me@juliankniephoff.com>
2022-09-27 18:08:03 +0200

    updates

---
+++ .cask/28.2/elpa/evil-20220926.1705/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20220926.1705" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "dba2fa9907cf096f5e615df2e8b0381b643d47ee" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulation" "vim")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
