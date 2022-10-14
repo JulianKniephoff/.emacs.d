2ab0ac43f75dcebb55bdcfdc1a884190878b5ccc
Julian Kniephoff <me@juliankniephoff.com>
2022-10-14 14:52:41 +0200

    updates

---
+++ .cask/28.2/elpa/cargo-20221012.1151/cargo-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cargo" "20221012.1151" "Emacs Minor Mode for Cargo, Rust's Package Manager."
+  '((emacs "24.3")
+    (markdown-mode "2.4"))
+  :commit "e946e365ad372139e05d29638dae96a52c6dbd70" :authors
+  '(("Kevin W. van Rooijen"))
+  :maintainer
+  '("Kevin W. van Rooijen")
+  :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
