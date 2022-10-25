fa9efa5cf1eb562d91c278a087f42da97381e6aa
Julian Kniephoff <me@juliankniephoff.com>
2022-10-25 14:23:30 +0200

    updates

---
+++ .cask/28.2/elpa/evil-20221024.1556/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20221024.1556" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "88da852e3df2fa6a4b03ea5ed7e6dc4a1aef6c15" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulation" "vim")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/28.2/elpa/lsp-mode-20221024.1146/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20221024.1146" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "af4bd151943e582a1269fd18bd4246b2d00ae55d" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
