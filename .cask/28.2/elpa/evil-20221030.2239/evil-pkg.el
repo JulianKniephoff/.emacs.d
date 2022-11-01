719bb5116253c47b226621c5be9c6516479b56b5
Julian Kniephoff <me@juliankniephoff.com>
2022-11-01 14:22:34 +0100

    updates

---
+++ .cask/28.2/elpa/evil-20221030.2239/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20221030.2239" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "26168f1972fdf263d964715862cb782b9b33b71d" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulation" "vim")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/28.2/elpa/php-mode-20221027.1651/php-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "php-mode" "20221027.1651" "Major mode for editing PHP code"
+  '((emacs "25.2"))
+  :commit "c3c442a3fa8714b17f166fb1f60f3326d583a22e" :authors
+  '(("Eric James Michael Ritz"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
