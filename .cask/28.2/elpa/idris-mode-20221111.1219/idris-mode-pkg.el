d939919e348de45edb8aecbf086f0c11a7eaf755
Julian Kniephoff <me@juliankniephoff.com>
2022-11-16 14:11:49 +0100

    updates

---
+++ .cask/28.2/elpa/idris-mode-20221111.1219/idris-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "idris-mode" "20221111.1219" "Major mode for editing Idris code"
+  '((emacs "24")
+    (prop-menu "0.1")
+    (cl-lib "0.5"))
+  :commit "ea83139b3981113f3b2115ea3c3efd3d7af6ca89" :keywords
+  '("languages")
+  :url "https://github.com/idris-hackers/idris-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
