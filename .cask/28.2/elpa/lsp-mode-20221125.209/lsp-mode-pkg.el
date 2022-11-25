696e2d51cb61b52136ec2cab87c4db05f2e5ce6c
Julian Kniephoff <me@juliankniephoff.com>
2022-11-25 20:34:07 +0100

    updates

---
+++ .cask/28.2/elpa/lsp-mode-20221125.209/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20221125.209" "LSP mode"
+  '((emacs "26.3")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "fad33b5f5ff1664a8d5b524fe2c2fa895d06b12d" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
