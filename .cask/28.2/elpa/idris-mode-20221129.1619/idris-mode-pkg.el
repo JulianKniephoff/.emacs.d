6d9db830b5f639a4477a5c74b182997625317267
Julian Kniephoff <me@juliankniephoff.com>
2022-11-30 12:48:57 +0100

    updates

---
+++ .cask/28.2/elpa/idris-mode-20221129.1619/idris-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "idris-mode" "20221129.1619" "Major mode for editing Idris code"
+  '((emacs "24")
+    (prop-menu "0.1")
+    (cl-lib "0.5"))
+  :commit "fdbd019cb284455f0e2e887a833f64c67b89c80f" :keywords
+  '("languages")
+  :url "https://github.com/idris-hackers/idris-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .cask/28.2/elpa/paredit-20221124.2249/paredit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from paredit.el  -*- no-byte-compile: t -*-
-(define-package "paredit" "20221124.2249" "minor mode for editing parentheses" 'nil :commit "b9e2e6a1420f0a800a60ea24779a82c0292fb0d5" :authors '(("Taylor R. Campbell" . "campbell@paredit.org")) :maintainer '("Taylor R. Campbell" . "campbell@paredit.org") :keywords '("lisp"))
