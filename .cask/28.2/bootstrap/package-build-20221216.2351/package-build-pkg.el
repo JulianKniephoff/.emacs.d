7a899a93641c425b764c4f2599b8e7bb57fc0a12
Julian Kniephoff <me@juliankniephoff.com>
2022-12-21 15:54:21 +0100

    updates

---
+++ .cask/28.2/bootstrap/package-build-20221216.2351/package-build-pkg.el
@@ -0,0 +1,15 @@
+(define-package "package-build" "20221216.2351" "Tools for assembling a package archive"
+  '((emacs "25.1"))
+  :commit "b800e47a9f7139bc0b26a1ad4a01d4ae52b089f0" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+    ("Steve Purcell" . "steve@sanityinc.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Phil Hagelberg" . "technomancy@gmail.com"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("maint" "tools")
+  :url "https://github.com/melpa/package-build")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/28.2/elpa/rust-mode-20221220.1926/rust-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "rust-mode" "20221220.1926" "A major-mode for editing Rust source code"
+  '((emacs "25.1"))
+  :commit "384051e23f0f2e2950e6ce2efd1a2e8ac0c53613" :authors
+  '(("Mozilla"))
+  :maintainer
+  '("Mozilla")
+  :keywords
+  '("languages")
+  :url "https://github.com/rust-lang/rust-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
