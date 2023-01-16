eb951dd3cd2276d3ebaac891e54c112f8ce8f167
Julian Kniephoff <me@juliankniephoff.com>
2023-01-16 11:43:02 +0100

    updates

---
+++ .cask/28.2/elpa/auctex-13.1.8/auctex-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from auctex.el  -*- no-byte-compile: t -*-
+(define-package "auctex" "13.1.8" "Integrated environment for *TeX*" '((emacs "25.1")) :commit "ebd714ef815b6ab8b9b947b884a210fa14df3a47" :maintainer '(nil . "auctex-devel@gnu.org") :keywords '("tex" "latex" "texinfo" "context" "doctex" "preview-latex") :url "https://www.gnu.org/software/auctex/")
---
+++ .cask/28.2/elpa/ledger-mode-20230106.1610/ledger-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ledger-mode" "20230106.1610" "Helper code for use with the \"ledger\" command-line tool"
+  '((emacs "25.1"))
+  :commit "4b32f701736b37f99048be79583b0bde7cc14c85")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
