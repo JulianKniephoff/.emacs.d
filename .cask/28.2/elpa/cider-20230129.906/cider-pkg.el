f495b2eca0c0895d3438bee77a0847b7623e0678
Julian Kniephoff <me@juliankniephoff.com>
2023-01-31 16:16:03 +0100

    updates

---
+++ .cask/28.2/elpa/cider-20230129.906/cider-pkg.el
@@ -0,0 +1,23 @@
+(define-package "cider" "20230129.906" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "26")
+    (clojure-mode "5.16.0")
+    (parseedn "1.0.6")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2"))
+  :commit "71a319e265aa7c014c3cb192f58e36c7691e63d2" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
