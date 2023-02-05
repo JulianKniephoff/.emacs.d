0efda56817c611dc96c54755f6a30567d3f2c240
Julian Kniephoff <me@juliankniephoff.com>
2023-02-05 21:38:17 +0100

    updates

---
+++ .cask/28.2/elpa/cider-20230205.643/cider-pkg.el
@@ -0,0 +1,23 @@
+(define-package "cider" "20230205.643" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "26")
+    (clojure-mode "5.16.0")
+    (parseedn "1.0.6")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2"))
+  :commit "ec31a4614bf612b228c79d59cc7554c4e9605db4" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/28.2/elpa/lsp-mode-20230203.924/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20230203.924" "LSP mode"
+  '((emacs "26.3")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "6b01d49757994c09c90623bf67f072d02f00f8e9" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
