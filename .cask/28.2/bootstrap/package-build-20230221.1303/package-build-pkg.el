b65347b3925d95589efa0aace192544d44c42f9b
Julian Kniephoff <me@juliankniephoff.com>
2023-03-01 12:38:04 +0100

    updates

---
+++ .cask/28.2/bootstrap/package-build-20230221.1303/package-build-pkg.el
@@ -0,0 +1,15 @@
+(define-package "package-build" "20230221.1303" "Tools for assembling a package archive"
+  '((emacs "26.1"))
+  :commit "9199271d78dc0f4f85047d6daef0e9721a6e63cd" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+    ("Steve Purcell" . "steve@sanityinc.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Phil Hagelberg" . "technomancy@gmail.com"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("maint" "tools")
+  :url "https://github.com/melpa/package-build")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .cask/28.2/elpa/cmake-mode-20230209.1921/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20230209.1921" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "29bcbcab4fe3750d7529f49e9d71768c76a23a47")
