58392943340a2847dd5e8aad76230bf0da07fdc8
Julian Kniephoff <me@juliankniephoff.com>
2023-03-06 09:48:57 +0100

    updates

---
+++ .cask/28.2/elpa/php-mode-20230305.2120/php-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "php-mode" "20230305.2120" "Major mode for editing PHP code"
+  '((emacs "25.2"))
+  :commit "a399e6a531255a2274db906cbb207e486f16b314" :authors
+  '(("Eric James Michael Ritz"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
