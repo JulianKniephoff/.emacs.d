9a5567cf30fc2ff75d8ccbf9eac5995cce469071
Julian Kniephoff <me@juliankniephoff.com>
2023-03-27 12:07:15 +0200

    updates

---
+++ .cask/28.2/bootstrap/package-build-20230319.1115/package-build-pkg.el
@@ -0,0 +1,15 @@
+(define-package "package-build" "20230319.1115" "Tools for assembling a package archive"
+  '((emacs "26.1"))
+  :commit "974406211f9c9d6549f80ca33f6ba8797cb2b11f" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+    ("Steve Purcell" . "steve@sanityinc.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Phil Hagelberg" . "technomancy@gmail.com"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("maint" "tools")
+  :url "https://github.com/melpa/package-build")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
