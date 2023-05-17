f8d4aae948e0b9f8519953458ebddc1bf418a8e5
Julian Kniephoff <me@juliankniephoff.com>
2023-05-17 15:39:27 +0200

    updates

---
+++ .cask/28.2/elpa/swift-mode-20230507.717/swift-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "swift-mode" "20230507.717" "Major-mode for Apple's Swift programming language"
+  '((emacs "24.4")
+    (seq "2.3"))
+  :commit "3efd087008fba52bd43008cbcbf513b3273ef030" :authors
+  '(("taku0" . "mxxouy6x3m_github@tatapa.org")
+    ("Chris Barrett" . "chris.d.barrett@me.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com")
+    ("Arthur Evstifeev" . "lod@pisem.net"))
+  :maintainers
+  '(("taku0" . "mxxouy6x3m_github@tatapa.org"))
+  :maintainer
+  '("taku0" . "mxxouy6x3m_github@tatapa.org")
+  :keywords
+  '("languages" "swift")
+  :url "https://github.com/swift-emacs/swift-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
