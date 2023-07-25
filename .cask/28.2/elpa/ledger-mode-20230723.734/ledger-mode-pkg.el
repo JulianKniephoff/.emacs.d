3f2e684a9899161067272554dadd092789fc7690
Julian Kniephoff <me@juliankniephoff.com>
2023-07-25 13:10:34 +0200

    updates

---
+++ .cask/28.2/elpa/ledger-mode-20230723.734/ledger-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ledger-mode" "20230723.734" "Helper code for use with the \"ledger\" command-line tool"
+  '((emacs "25.1"))
+  :commit "d102ba5ec7b5f19970bb3627d400473b525b7a4b")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/28.2/elpa/lsp-mode-20230721.1656/lsp-mode-pkg.el
@@ -0,0 +1,21 @@
+(define-package "lsp-mode" "20230721.1656" "LSP mode"
+  '((emacs "26.3")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "db0b3789b1d6a4b788e8cc37f8dbd45e2c592ad7" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainers
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
