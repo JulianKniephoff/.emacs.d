e466a4b95bfd4eea827eaab81cd773d3f4a52b71
Julian Kniephoff <me@juliankniephoff.com>
2023-11-25 13:31:35 +0100

    updates

---
+++ .cask/29.1/elpa/cmake-mode-20231115.1355/cmake-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "cmake-mode" "20231115.1355" "major-mode for editing CMake sources"
+  '((emacs "24.1"))
+  :commit "9cfff766eb26ab8b29aa509e42d6b367dd00c7b7")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
