863e92bdda56d53988100f64126dda63e67ce2a7
Julian Kniephoff <me@juliankniephoff.com>
2023-12-21 15:35:28 +0100

    updates

---
+++ .cask/29.1/elpa/evil-collection-20231219.1501/evil-collection-pkg.el
@@ -0,0 +1,16 @@
+(define-package "evil-collection" "20231219.1501" "A set of keybindings for Evil mode"
+  '((emacs "26.3")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "c30158a0df2e252f15cffcd61ceb0ad58a395096" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .cask/29.1/elpa/ledger-mode-20230925.1013/ledger-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "ledger-mode" "20230925.1013" "Helper code for use with the \"ledger\" command-line tool"
-  '((emacs "25.1"))
-  :commit "105d67626e91acb4550ab860e0dc4247c8a4c79e")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
