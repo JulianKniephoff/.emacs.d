57cc8019aa65e8aba4da1f6cd37f429275c9c2f5
Julian Kniephoff <me@juliankniephoff.com>
2024-02-18 17:06:53 +0100

    updates

--- .cask/29.1/elpa/ledger-mode-20231216.818/ledger-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "ledger-mode" "20231216.818" "Helper code for use with the \"ledger\" command-line tool"
-  '((emacs "25.1"))
-  :commit "9a7b69ceb29bc5b530b9718a162586ea523f7820")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
