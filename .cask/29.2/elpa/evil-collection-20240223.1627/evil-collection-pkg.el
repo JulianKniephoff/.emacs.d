983b4b846a48f810b42dc92fd4fb4b2f7e86d2e8
Julian Kniephoff <me@juliankniephoff.com>
2024-03-04 09:52:33 +0100

    updates

---
+++ .cask/29.2/elpa/evil-collection-20240223.1627/evil-collection-pkg.el
@@ -0,0 +1,16 @@
+(define-package "evil-collection" "20240223.1627" "A set of keybindings for Evil mode"
+  '((emacs "26.3")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "54e1eb02d918fb38b4e71c87ea9a8fe8f499dc5f" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
