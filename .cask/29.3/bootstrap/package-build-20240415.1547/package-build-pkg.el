32d4c5b04ea7dd84f89af2e104d2735a59fb18b5
Julian Kniephoff <me@juliankniephoff.com>
2024-04-22 23:45:38 +0200

    updates

---
+++ .cask/29.3/bootstrap/package-build-20240415.1547/package-build-pkg.el
@@ -0,0 +1,18 @@
+(define-package "package-build" "20240415.1547" "Tools for assembling a package archive"
+  '((emacs "26.1")
+    (compat "27.1"))
+  :commit "d9ce37b82529827ce720ad3ccf4af887d19fcfb0" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+    ("Steve Purcell" . "steve@sanityinc.com")
+    ("Jonas Bernoulli" . "emacs.package-build@jonas.bernoulli.dev")
+    ("Phil Hagelberg" . "technomancy@gmail.com"))
+  :maintainers
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("maint" "tools")
+  :url "https://github.com/melpa/package-build")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .cask/29.3/elpa/evil-20240421.1915/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20240421.1915" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "fe44a565e6806dba4c298a76eb8800063016ec2d" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
